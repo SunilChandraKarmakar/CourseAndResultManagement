976b946fef993dc77884a601f0b787f0201ad8c6
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-16 11:28:36 +0600

    Create department controller and index function

---
+++ CourseAndResultMS/CourseAndResultMS/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using CourseAndResultMS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CourseAndResultMS.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
+
+        public ActionResult Index()
+        {
+            List<Department> departments = db.Departments.ToList();
+            ViewBag.DepartmentList = departments;
+            return View();
+        }
+    }
+}
