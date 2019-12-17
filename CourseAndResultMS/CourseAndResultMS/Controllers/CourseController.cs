26c3a2283e3427a27158468a31c0875fc699434c
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-17 21:36:20 +0600

    Update course mode, update isexits function, write create post function

--- CourseAndResultMS/CourseAndResultMS/Controllers/CourseController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/CourseController.cs
@@ -17,11 +17,21 @@ namespace CourseAndResultMS.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            List<Department> departments = db.Departments.ToList();
+            List<Semester> semesters = db.Semesters.ToList();
+            ViewBag.DepartmentList = departments;
+            ViewBag.SemesterList = semesters;
+            return View();
+        }
+
         public JsonResult CodeIsExist(string code)
         {
             Course course = db.Courses.FirstOrDefault(c => c.Code == code);
 
-            if (code != null)
+            if (course != null)
                 return Json(1);
             else
                 return Json(0);
@@ -31,20 +41,27 @@ namespace CourseAndResultMS.Controllers
         {
             Course course = db.Courses.FirstOrDefault(c => c.Name == name);
 
-            if (name != null)
+            if (course != null)
                 return Json(1);
             else
                 return Json(0);
         }
 
-        [HttpGet]
-        public ActionResult Create()
+        [HttpPost]
+        public ActionResult Create(Course aCourse)
         {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(aCourse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
             List<Department> departments = db.Departments.ToList();
             List<Semester> semesters = db.Semesters.ToList();
             ViewBag.DepartmentList = departments;
             ViewBag.SemesterList = semesters;
-            return View();
+            return View(aCourse);
         }
     }
 }
