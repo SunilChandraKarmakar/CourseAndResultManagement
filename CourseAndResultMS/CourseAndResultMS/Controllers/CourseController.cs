71879764889d1d62a4cbaf765e08a4b57a5b88f4
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-17 17:59:46 +0600

    Create Index get function and Index function view for course

---
+++ CourseAndResultMS/CourseAndResultMS/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using CourseAndResultMS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CourseAndResultMS.Controllers
+{
+    public class CourseController : Controller
+    {
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Course> courses = db.Courses.ToList();
+            ViewBag.CourseList = courses;
+            return View();
+        }
+    }
+}
