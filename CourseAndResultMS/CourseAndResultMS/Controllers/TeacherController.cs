781dade96d2f4a4cdade945af4c0b2767184bbb0
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-18 01:12:15 +0600

    Create Teacher Index function and view

--- CourseAndResultMS/CourseAndResultMS/Controllers/TeacherController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/TeacherController.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using CourseAndResultMS.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CourseAndResultMS.Controllers
 {
     public class TeacherController : Controller
     {
-        // GET: Teacher
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
+
+        [HttpGet]
         public ActionResult Index()
         {
+            List<Teacher> teachers = db.Teachers.ToList();
+            ViewBag.TeacherList = teachers;
             return View();
         }
     }
