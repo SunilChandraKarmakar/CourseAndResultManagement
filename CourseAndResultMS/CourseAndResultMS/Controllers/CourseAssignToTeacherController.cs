4edcb99dc6cd6401ca929a4cc03dd990a1cee74c
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-18 16:06:13 +0600

    Create Index function and view for CourseAssignToTeacher

--- CourseAndResultMS/CourseAndResultMS/Controllers/CourseAssignToTeacherController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/CourseAssignToTeacherController.cs
@@ -1,9 +1,20 @@
-﻿using System.Web.Mvc;
+﻿using CourseAndResultMS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CourseAndResultMS.Controllers
 {
     public class CourseAssignToTeacherController : Controller
     {
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
 
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<CourseAssignToTeacher> courseAssignToTeachers = db.CourseAssignToTeachers.ToList();
+            ViewBag.CourseAssignToTeachers = courseAssignToTeachers;
+            return View();
+        }
     }
 }
