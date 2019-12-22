a2bb13990083fac28bce68fe971af4e01a496bed
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-22 23:23:23 +0600

    Create RegisterStudentController with Index function

---
+++ CourseAndResultMS/CourseAndResultMS/Controllers/RegisterStudentController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CourseAndResultMS.Controllers
+{
+    public class RegisterStudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
