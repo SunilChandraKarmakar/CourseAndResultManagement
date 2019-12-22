373d4df7d5c7f2116f3222c6bf384063285febab
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-23 02:11:44 +0600

    Create index functuion and view for RegisterStudent

--- CourseAndResultMS/CourseAndResultMS/Controllers/RegisterStudentController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/RegisterStudentController.cs
@@ -1,12 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using CourseAndResultMS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CourseAndResultMS.Controllers
 {
     public class RegisterStudentController : Controller
     {
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
+
         [HttpGet]
         public ActionResult Index()
         {
+            List<RegisterStudent> registerStudents = db.RegisterStudents.ToList();
+            ViewBag.RegisterStudentList = registerStudents;
             return View();
         }
     }
