28e271cd8a3a3325cfea55ce6b4d4c84c67546bb
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-24 13:10:54 +0600

    Create AllocateClassRoom Index function

--- CourseAndResultMS/CourseAndResultMS/Controllers/AllocateClassRoomController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/AllocateClassRoomController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using CourseAndResultMS.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CourseAndResultMS.Controllers
 {
     public class AllocateClassRoomController : Controller
     {
-        // GET: AllocateClassRoom
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
+
+        [HttpGet]
         public ActionResult Index()
         {
+            List<Department> departments = db.Departments.ToList();
+            List<Course> courses = db.Courses.ToList();
+            List<ClassRoom> classRooms = db.ClassRooms.ToList();
+            List<Week> weeks = db.Weeks.ToList();
+
+            ViewBag.DepartmentList = departments;
+            ViewBag.CourseList = courses;
+            ViewBag.ClassRoomList = classRooms;
+            ViewBag.WeekList = weeks;
+
             return View();
         }
     }
