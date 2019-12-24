46cd16c38690b06924121b615828de7a1f01e047
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-24 21:48:34 +0600

    working with Create get function and view

--- CourseAndResultMS/CourseAndResultMS/Controllers/AllocateClassRoomController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/AllocateClassRoomController.cs
@@ -13,12 +13,18 @@ namespace CourseAndResultMS.Controllers
         public ActionResult Index()
         {
             List<AllocateClassRoom> allocateClassRooms = db.AllocateClassRooms.ToList();
+            ViewBag.AllocateClassRoom = allocateClassRooms;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             List<Department> departments = db.Departments.ToList();
             List<Course> courses = db.Courses.ToList();
             List<ClassRoom> classRooms = db.ClassRooms.ToList();
             List<Week> weeks = db.Weeks.ToList();
 
-            ViewBag.AllocateClassRoom = allocateClassRooms;
             ViewBag.DepartmentList = departments;
             ViewBag.CourseList = courses;
             ViewBag.ClassRoomList = classRooms;
