35fb5f6d6663e468aa4e122f7a0e83c7d2503414
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-31 19:40:41 +0600

    Get student name, email, department and courseList by RegisterStudentId using ajax

--- CourseAndResultMS/CourseAndResultMS/Controllers/StudentResultController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/StudentResultController.cs
@@ -21,5 +21,38 @@ namespace CourseAndResultMS.Controllers
             ViewBag.GradeLetterList = gradeLetters;
             return View();
         }
+
+        public JsonResult GetRegisterStudentInfo(int registerStudentId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<RegisterStudent> registerStudents = db.RegisterStudents.ToList();
+            RegisterStudent getAStudentInfo = registerStudents.Find(r => r.RegisterStudentId == registerStudentId);
+            return Json(getAStudentInfo, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetDepartmentByRegistrationId(int registerStudentId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<RegisterStudent> registerStudents = db.RegisterStudents.ToList();
+            RegisterStudent getRegisterStudent = registerStudents.Find(r => r.RegisterStudentId == registerStudentId);
+            int departmentId = getRegisterStudent.DepartmentId;
+
+            List<Department> departments = db.Departments.ToList();
+            Department getDepartment = departments.Find(d => d.DepartmentId == departmentId);
+            string departmentName = getDepartment.Name;
+            return Json(departmentName, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetCourseListByRegisterStudentId(int registerStudentId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<RegisterStudent> registerStudents = db.RegisterStudents.ToList();
+            RegisterStudent getARegisterStudent = registerStudents.Find(r => r.RegisterStudentId == registerStudentId);
+            int studentDepartmentId = getARegisterStudent.DepartmentId;
+
+            List<Course> courses = db.Courses.ToList();
+            List<Course> getStudentCourseByDepartmentId = courses.FindAll(c => c.DepartmentId == studentDepartmentId);
+            return Json(getStudentCourseByDepartmentId, JsonRequestBehavior.AllowGet);
+        }
     }
 }
