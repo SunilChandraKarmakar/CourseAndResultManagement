9d4ed409757a7fc7bb42e0185a0d530e774b9ddc
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-12-31 17:16:08 +0600

    Working with Create function and view for StudentResult controller

--- CourseAndResultMS/CourseAndResultMS/Controllers/StudentResultController.cs
+++ CourseAndResultMS/CourseAndResultMS/Controllers/StudentResultController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CourseAndResultMS.Models;
 
 namespace CourseAndResultMS.Controllers
 {
     public class StudentResultController : Controller
     {
-        
+        private CourseAndResultManagementEntities db = new CourseAndResultManagementEntities();
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            List<RegisterStudent> registerStudents = db.RegisterStudents.ToList();
+            ViewBag.RegisterStudentList = registerStudents;
+
+            List<GradeLetter> gradeLetters = db.GradeLetters.ToList();
+            ViewBag.GradeLetterList = gradeLetters;
+            return View();
+        }
     }
 }
